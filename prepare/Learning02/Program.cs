787d9660dfff7dacbf5d921ad2b0d7515bc356d1
Yvescyril <cyrilyves61@gmail.com>
2023-01-21 21:16:52 -0600

    abtraction

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,13 +4,24 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.Write("What is your first name ? ");
-        string first = Console.ReadLine();
+        Job job1 = new Job();
+        job1._jobTitle = "Software Engineer";
+        job1._company = "Microsoft";
+        job1._startYear = 2019;
+        job1._endYear = 2022;
 
-        Console.Write("What is your last name ? ");
-        string last = Console.ReadLine();
+        Job job2 = new Job();
+        job2._jobTitle = "Manager";
+        job2._company = "Apple";
+        job2._startYear = 2022;
+        job2._endYear = 2023;
 
-        Console.WriteLine($"Your name is {last}, {first} {last}");
+        Resume myResume = new Resume();
+        myResume._name = "Allison Rose";
 
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+
+        myResume.Display();
     }
 }
